5f01080378aac2f033d5a5c677ba47772e0a9b35
sol_lee <96866819+sol-namoo@users.noreply.github.com>
2022-11-01 14:27:11 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,12 +1,15 @@
+// function solution(age) {
+//     var answer = getAlpha(age);
+//     function getAlpha(age){
+//         if(age < 10) return String.fromCharCode(97 + age)
+//         return getAlpha(Math.floor(age/10)) + String.fromCharCode(97 + age % 10)
+//     }
+//     return answer;
+// }
+
 function solution(age) {
-    var answer = getAlpha(age);
-    function getAlpha(age){
-        if(age < 10) return String.fromCharCode(97 + age)
-        return getAlpha(Math.floor(age/10)) + String.fromCharCode(97 + age % 10)
-    }
+    var answer = '';
+    for (char of String(age)) answer += String.fromCharCode(97 + Number(char))
     return answer;
 }
 
-// a의 유니코드는 97 => 문자.charCodeAt(0) - 97 하면 10진수 숫자
-// 23살 => '2'+'3' => 'c'+'d'
-// console.log(String.fromCharCode(97 + 2))
